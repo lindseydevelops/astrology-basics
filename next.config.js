b5779aacea586af477a5e4b89b5f6e735d997992
Lindsey Shelton <lindseycreeps@gmail.com>
2023-11-18 16:53:28 -0600

    Github Workflow Test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,27 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: "imgix",
+    path: 'the "domain" of your Imigix source',
   },
 };
 
